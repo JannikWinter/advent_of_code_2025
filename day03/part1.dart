c07e0083ddedc3f8bb7c69fbc2c858ddd3b68aec
Jannik Winter <kontakt@jannik-winter.com>
2025-12-04 19:57:24 +0100

    solve day 3

---
+++ day03/part1.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+typedef BankJoltage = ({int joltage, int firstIndex, int lastIndex});
+
+void main() {
+  final File file = File("./day03/input");
+  final List<String> lines = file.readAsLinesSync();
+
+  List<int> bankJoltageValues = [];
+  for (final String bank in lines) {
+    final List<BankJoltage> currentBankJoltages =
+        {for (int i = 0; i < bank.length; i++) bank.substring(i, i + 1)}
+            .map(
+              (joltageStr) => (
+                joltage: int.parse(joltageStr),
+                firstIndex: bank.indexOf(joltageStr),
+                lastIndex: bank.lastIndexOf(joltageStr),
+              ),
+            )
+            .toList();
+    currentBankJoltages.sort((e1, e2) => e2.joltage.compareTo(e1.joltage));
+
+    outer:
+    for (BankJoltage firstJoltage in currentBankJoltages) {
+      for (BankJoltage secondJoltage in currentBankJoltages) {
+        if (firstJoltage.firstIndex < secondJoltage.lastIndex) {
+          bankJoltageValues.add(
+            firstJoltage.joltage * 10 + secondJoltage.joltage,
+          );
+          break outer;
+        }
+      }
+    }
+  }
+
+  final int sum = bankJoltageValues.reduce((value, element) => value + element);
+  print(sum);
+}
---
+++ day03/part2.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+const int nDigits = 12;
+
+void main() {
+  final File file = File("./day03/input");
+  final List<String> lines = file.readAsLinesSync();
+
+  List<int> resultValues = [];
+  for (final String bank in lines) {
+    final List<int> currentBankJoltages = [
+      for (int i = 0; i < bank.length; i++) int.parse(bank.substring(i, i + 1)),
+    ];
+    final List<int> resultDigits = [];
+    int lastDigitIndex = -1;
+
+    while (resultDigits.length < nDigits) {
+      final (int largestDigit, int largestDigitIndex) = getLargestJoltage(
+        currentBankJoltages,
+        start: lastDigitIndex + 1,
+        end: bank.length - (nDigits - resultDigits.length) + 1,
+      );
+      resultDigits.add(largestDigit);
+      lastDigitIndex = largestDigitIndex;
+    }
+
+    resultValues.add(int.parse(resultDigits.join()));
+  }
+
+  final int sum = resultValues.reduce((value, element) => value + element);
+  print(sum);
+}
+
+/// [start] inclusive, [end] exclusive
+(int, int) getLargestJoltage(List<int> joltages, {int? start, int? end}) {
+  int largestJoltage = 0;
+  int largestJoltageIndex = -1;
+
+  for (int i = start ?? 0; i < (end ?? joltages.length); i++) {
+    final int digit = joltages[i];
+    if (digit > largestJoltage) {
+      largestJoltage = digit;
+      largestJoltageIndex = i;
+    }
+  }
+
+  return (largestJoltage, largestJoltageIndex);
+}
