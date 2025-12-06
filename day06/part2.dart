e57abe17f3c4a24ae91b9b51afb5b43177609376
Jannik Winter <jannik9797@web.de>
2025-12-06 17:58:14 +0100

    solve day 6 part 2

--- day06/part2.dart
+++ day06/part2.dart
@@ -1,6 +1,57 @@
 import 'dart:io';
 
+import '../util.dart';
+
 void main() {
-  final File file = File("./day06/test_input");
-  final List<String> lines = file.readAsLinesSync();
+  final File file = File("./day06/input");
+  final List<List<String>> charactersByRow = file
+      .readAsLinesSync()
+      .map((line) => line.chars.toList())
+      .toList();
+
+  final int nRows = charactersByRow.length;
+  final int nColumns = charactersByRow[0].length;
+
+  int result = 0;
+
+  String currentOperator = "";
+  final List<int> currentBlockNumbers = [];
+  for (int x = 0; x < nColumns; x++) {
+    final String opChar = charactersByRow.last[x];
+    if (opChar != " ") {
+      currentOperator = opChar;
+    }
+
+    final List<String> digits = [];
+    for (int y = 0; y < nRows - 1; y++) {
+      final String digit = charactersByRow[y][x];
+      digits.add(digit);
+    }
+
+    final String currentNumberStr = digits.join().trim();
+    if (currentNumberStr.isNotEmpty) {
+      final int currentNumber = int.parse(currentNumberStr);
+      currentBlockNumbers.add(currentNumber);
+    } else {
+      final int currentBlockResult;
+      switch (currentOperator) {
+        case "+":
+          currentBlockResult = currentBlockNumbers.reduce(
+            (value, element) => value + element,
+          );
+        case "*":
+          currentBlockResult = currentBlockNumbers.reduce(
+            (value, element) => value * element,
+          );
+        default:
+          throw Error();
+      }
+      result += currentBlockResult;
+
+      currentOperator = "";
+      currentBlockNumbers.clear();
+    }
+  }
+
+  print(result);
 }
--- util.dart
+++ util.dart
@@ -1,7 +1,9 @@
 extension StringChars on String {
-  Iterable<String> get chars => [
-    for (int i = 0; i < length; i++) substring(i, i + 1),
-  ];
+  Iterable<String> get chars => [for (int i = 0; i < length; i++) charAt(i)];
+
+  String charAt(int index) {
+    return substring(index, index + 1);
+  }
 }
 
 const int maxInteger = 0x7FFFFFFFFFFFFFFF;
