084f5c215a380be731f66be63acacca9047c240c
Jannik Winter <kontakt@jannik-winter.com>
2025-12-11 22:02:30 +0100

    start day 10 part 2

--- day10/part2.dart
+++ day10/part2.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
+
 void main() {
   final File file = File("./day10/input");
   final int result = solveDay10Part2(file);
@@ -7,5 +9,121 @@ void main() {
 }
 
 int solveDay10Part2(File file) {
-  throw UnimplementedError();
+  final List<String> lines = file.readAsLinesSync();
+
+  int result = 0;
+  for (final String line in lines) {
+    final [_, ...buttonStrs, joltagesStr] = line.split(" ");
+
+    final List<_Button> buttons = buttonStrs
+        .map((buttonStr) => _Button.fromString(buttonStr))
+        .toList();
+
+    final _JoltageConfiguration targetJoltage =
+        _JoltageConfiguration.fromString(joltagesStr);
+
+    final Set<_JoltageConfiguration> unprocessedJoltages = {
+      _JoltageConfiguration(List.filled(targetJoltage.length, 0)),
+    };
+
+    int? nPresses;
+
+    outer:
+    for (int i = 0; unprocessedJoltages.isNotEmpty; i++) {
+      final Set<_JoltageConfiguration> processingJoltages = Set.from(
+        unprocessedJoltages,
+      );
+      unprocessedJoltages.clear();
+
+      for (final _JoltageConfiguration joltage in processingJoltages) {
+        if (joltage == targetJoltage) {
+          nPresses = i;
+          break outer;
+        }
+        if (joltage > targetJoltage) continue;
+
+        for (final _Button button in buttons) {
+          unprocessedJoltages.add(joltage + button);
+        }
+      }
+    }
+
+    assert(nPresses != null);
+
+    print("$joltagesStr: $nPresses");
+
+    result += nPresses!;
+  }
+
+  return result;
+}
+
+class _Button {
+  _Button(Iterable<int> triggeredIndices)
+    : _triggeredIndices = Set.from(triggeredIndices);
+  _Button.fromString(String buttonString)
+    : _triggeredIndices = buttonString
+          .substring(1, buttonString.length - 1)
+          .split(",")
+          .map((idxStr) => int.parse(idxStr))
+          .toSet();
+
+  final Set<int> _triggeredIndices;
+
+  @override
+  bool operator ==(covariant _Button other) {
+    return SetEquality(
+      IdentityEquality(),
+    ).equals(_triggeredIndices, other._triggeredIndices);
+  }
+
+  @override
+  int get hashCode => Object.hashAll(_triggeredIndices);
+}
+
+class _JoltageConfiguration {
+  _JoltageConfiguration(Iterable<int> values) : _values = List.from(values);
+  _JoltageConfiguration.fromString(String joltageString)
+    : _values = joltageString
+          .substring(1, joltageString.length - 1)
+          .split(",")
+          .map((joltageStr) => int.parse(joltageStr))
+          .toList();
+
+  final List<int> _values;
+
+  int get length => _values.length;
+
+  _JoltageConfiguration operator +(_Button button) {
+    final List<int> newValues = List.from(_values);
+    for (final int index in button._triggeredIndices) {
+      newValues[index]++;
+    }
+    return _JoltageConfiguration(newValues);
+  }
+
+  bool operator >(_JoltageConfiguration other) {
+    assert(length == other.length);
+    for (int i = 0; i < _values.length; i++) {
+      if (_values[i] <= other._values[i]) return false;
+    }
+
+    return true;
+  }
+
+  bool operator <(_JoltageConfiguration other) {
+    assert(length == other.length);
+    for (int i = 0; i < _values.length; i++) {
+      if (_values[i] >= other._values[i]) return false;
+    }
+    return true;
+  }
+
+  @override
+  bool operator ==(covariant _JoltageConfiguration other) {
+    return ListEquality(IdentityEquality()).equals(_values, other._values);
+  }
+
+  @override
+  int get hashCode => Object.hashAll(_values);
 }
--- test/day10/day10_test.dart
+++ test/day10/day10_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:test/test.dart';
 
 import '../../day10/part1.dart';
+import '../../day10/part2.dart';
 
 void main() {
   final File testFile = File("./test/day10/test_input");
@@ -10,8 +11,8 @@ void main() {
     test("Day 10, Part 1", () {
       expect(solveDay10Part1(testFile), 7);
     });
-    // test("Day 10, Part 2", () {
-    //   expect(solveDay10Part2(testFile), );
-    // });
+    test("Day 10, Part 2", () {
+      expect(solveDay10Part2(testFile), 33);
+    });
   });
 }
